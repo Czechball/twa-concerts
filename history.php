b9560975e84366b8701bcd3aea3c180287235d7b
Czechball <david@jenne.cz>
2020-02-03 16:04:31 +0100

    added some basics

--- history.php
+++ history.php
@@ -15,7 +15,20 @@ if (!is_numeric($year))
 		<title>Shows history</title>
 	</head>
 	<body>
-		
+		<h1>Show me da history</h1>
+		<h2>
+			<?php
+				echo '<a href="?season=';
+				echo $year -1;
+				echo '">';
+				echo $year - 1;
+				echo '</a>';
+				echo " ";
+				echo $year;
+				echo " ";
+				echo $year - 1;
+			?>
+		</h2>
 
 	</body>
 </html>
