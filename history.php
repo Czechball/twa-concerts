cd87b030232e778d2c083613d93ef3d53164bf49
Czechball <david@jenne.cz>
2020-02-03 15:39:50 +0100

    initial commit

---
+++ history.php
@@ -0,0 +1,21 @@
+<?php
+//$year = date("Y");
+$year = $_GET["season"];
+
+if (!is_numeric($year))
+{
+	$year = date("Y");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+
+	<head>
+		<title>Shows history</title>
+	</head>
+	<body>
+		
+
+	</body>
+</html>
